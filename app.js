82cdc128b7c9913c3d2b9232aa4badca23ec269c
Michael <mbg@outlook.com>
2013-09-29 15:02:31 +0100

    Added reference to https

--- app.js
+++ app.js
@@ -116,14 +116,14 @@ app.post('/login',
 
 app.get('/logout', removeUser, user.logout);
 
-//var options = {
-  //key: fs.readFileSync('./config/domain.pem'),
-  //cert: fs.readFileSync('./config/main.pem'),
-  //ca: [fs.readFileSync('./config/intermediate.pem')]
-//};
+var options = {
+  key: fs.readFileSync('./config/domain.pem'),
+  cert: fs.readFileSync('./config/main.pem'),
+  ca: [fs.readFileSync('./config/intermediate.pem')]
+};
 
 // Create an HTTP service.
 http.createServer(app).listen(5000);
 // Create an HTTPS service identical to the HTTP service.
-//https.createServer(options, app).listen(5001);
+https.createServer(options, app).listen(5001);
 
